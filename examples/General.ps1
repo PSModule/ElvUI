df34f061058a24e0e0da5e883b9cb2368b3b7cd5
Marius Storhaug <marstor@hotmail.com>
2026-04-19 22:33:13 +0200

    Add usage examples

---
+++ examples/General.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    .SYNOPSIS
+    Examples of how to use the ElvUI module.
+#>
+
+# Import the module
+Import-Module -Name ElvUI
+
+# Update ElvUI to the latest version
+Update-ElvUI
+
+# Force reinstall even if already up to date
+Update-ElvUI -Force
+
+# Install ElvUI fresh
+Install-ElvUI
+
+# Target a specific WoW installation and game flavor
+Update-ElvUI -WoWPath 'D:\Games\World of Warcraft' -Flavor '_classic_'
