858f7e233510775bca0b2c5ade6834e0f9cfdec4
Marius Storhaug <marstor@hotmail.com>
2026-04-19 22:33:04 +0200

    Add Pester tests for Install-ElvUI and Update-ElvUI

---
+++ tests/ElvUI.Tests.ps1
@@ -0,0 +1,38 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'ElvUI' {
+    Context 'Install-ElvUI' {
+        It 'Should be available' {
+            Get-Command Install-ElvUI -ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
+        }
+        It 'Should have WoWPath parameter' {
+            (Get-Command Install-ElvUI).Parameters.ContainsKey('WoWPath') | Should -BeTrue
+        }
+        It 'Should have Flavor parameter' {
+            (Get-Command Install-ElvUI).Parameters.ContainsKey('Flavor') | Should -BeTrue
+        }
+    }
+    Context 'Update-ElvUI' {
+        It 'Should be available' {
+            Get-Command Update-ElvUI -ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
+        }
+        It 'Should have WoWPath parameter' {
+            (Get-Command Update-ElvUI).Parameters.ContainsKey('WoWPath') | Should -BeTrue
+        }
+        It 'Should have Flavor parameter' {
+            (Get-Command Update-ElvUI).Parameters.ContainsKey('Flavor') | Should -BeTrue
+        }
+        It 'Should have Force parameter' {
+            (Get-Command Update-ElvUI).Parameters.ContainsKey('Force') | Should -BeTrue
+        }
+    }
+}
